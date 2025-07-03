e7c0611f79f919a31d81d6d932f6606a33a18824
Yaslim2 <yaslimsoares15@gmail.com>
2025-07-03 18:56:50 -0300

    fix: entities

--- src/entities/user-entity.ts
+++ src/entities/user-entity.ts
@@ -1,12 +1,3 @@
-import { generateUuid } from "@/shared/uuid"
+import { User } from "@prisma/client";
 
-export class UserEntity {
-  public readonly id: string
-  public readonly createdAt: Date
-  public updatedAt: Date
-  constructor(public name: string, public email: string, id?: string, createdAt?: Date, updatedAt?: Date) {
-    this.createdAt = createdAt ?? new Date()
-    this.updatedAt = updatedAt ?? new Date()
-    this.id = id ?? generateUuid()
-  }
-}
+export type UserEntity = User;
--- src/framework/repositories/mongo-user-repository.ts
+++ src/framework/repositories/mongo-user-repository.ts
@@ -1,53 +1,43 @@
-import { prisma } from "@/framework/database/prisma"
-import { UserEntity } from "@/entities/user-entity"
-import { InputFindBy, InputUpdateFields, IUserRepository } from "@/business/repositories/user-repository"
-import { Prisma, User } from "@/framework/database/prisma/config"
+import { prisma } from "@/framework/database/prisma";
+import { UserEntity } from "@/entities/user-entity";
+import {
+  InputFindBy,
+  InputUpdateFields,
+  IUserRepository,
+} from "@/business/repositories/user-repository";
+import { Prisma, User } from "@prisma/client";
 
 export class PrismaUserRepository implements IUserRepository {
   async create(input: { user: UserEntity }): Promise<UserEntity> {
-    const user = input.user
+    const user = input.user;
 
     await prisma.user.create({
       data: user,
-    })
+    });
 
-    return user
+    return user;
   }
 
   async list(): Promise<UserEntity[]> {
-    const users: User[] = await prisma.user.findMany()
+    const users: User[] = await prisma.user.findMany();
 
-    return users.map(user =>
-      new UserEntity(
-        user.name,
-        user.email,
-        user.id,
-        user.createdAt,
-        user.updatedAt
-      )
-    )
+    return users;
   }
 
   async getBy(input: InputFindBy): Promise<UserEntity> {
     const user = await prisma.user.findUniqueOrThrow({
       where: input as Prisma.UserWhereUniqueInput,
-    })
+    });
 
-    return new UserEntity(
-      user.name,
-      user.email,
-      user.id,
-      user.createdAt,
-      user.updatedAt
-    )
+    return user;
   }
 
   async update(input: InputUpdateFields): Promise<UserEntity> {
     const user = await prisma.user.update({
       where: { id: input.id },
       data: input.dataToUpdate,
-    })
+    });
 
-    return user
+    return user;
   }
 }
