c18fe5af931a46840733a2188bc4220dde9e044e
Emerson <emersonlucenaoficial@gmail.com>
2025-07-03 20:11:27 -0300

    fix type issue on users route file

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,7 @@
 import { UserController } from "@/controllers/user-controller";
+import { NextRequest } from "next/server";
 
 const controller = new UserController();
 
 export const GET = () => controller.list();
-export const POST = (req: Request) => controller.create(req);
+export const POST = (req: NextRequest) => controller.create(req);
